466fabdaccd86a81135f69814a1a239aa3e7b16f
JoshuaGuevaraJ1 <joguevara84@gmail.com>
2023-11-02 17:51:04 -0600

    Agregamos los modelos para las entidades de Tipos de animales y sus razas

--- catalogos/models.py
+++ catalogos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class TipoAnimal(models.Model):
+    tipoAnimal = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return self.tipoAnimal
+
+class Razas(models.Model):
+    raza = models.CharField(max_length=50, null=False)
+    tipoAnimal = models.ForeignKey(TipoAnimal, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "%s - %s" % (self.tipoAnimal, self.raza)
+    
